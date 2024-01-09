8691c98a35c1bd51578eb3dfeefd2a0edfd42958
mrykse <meryem.kose@hotmail.fr>
2024-01-09 17:47:08 +0100

    update

--- test_endtoend_app.py
+++ test_endtoend_app.py
@@ -11,6 +11,8 @@ class TestAppE2E(unittest.TestCase):
     def setUp(self):
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get('http://localhost:5000')
 
